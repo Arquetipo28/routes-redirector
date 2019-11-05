99be8395ab6071fe554bc2b9e21519e2617aac0c
German Reynaga <german.reynagaa@gmail.com>
2019-11-05 10:00:03 -0600

    Redirect all routes to skydropx

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express'),
       app = express();
 
+const unmatchedRoutes = (req, res) => {
+  res.redirect("https://app.skydropx.com");
+}
+
+app.use(unmatchedRoutes);
+
 app.get('/', (req, res) => {
   res.send('Route path')
 })
